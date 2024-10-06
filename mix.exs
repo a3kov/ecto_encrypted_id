4b75a5282b9698aac8d5bfdf036bee67ca6c8b63
Andrey Tretyakov <813860+a3kov@users.noreply.github.com>
2024-10-06 20:42:49 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,30 @@
 defmodule EctoEncryptedId.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/a3kov/ecto_encrypted_id"
+
   def project do
     [
       app: :ecto_encrypted_id,
-      version: "0.1.0",
-      elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: @version,
+      elixir: "~> 1.12",
+      deps: deps(),
+      source_url: @source_url,
+
+      # Hex
+      description: "Encrypted integer id fields for Ecto schemas",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => @source_url}
+      ],
+
+      # Docs
+      name: "EctoEncryptedId",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,7 +40,8 @@ defmodule EctoEncryptedId.MixProject do
     [
       {:ecto_sql, "~> 3.0"},
       {:base62, "~> 1.2"},
-      {:stream_data, "~> 1.0", only: :test}
+      {:stream_data, "~> 1.0", only: :test},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
 end
